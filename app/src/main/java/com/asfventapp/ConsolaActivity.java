47a62b7ce362b5f494481ffeaf9d199a1f3cb459
Emilio Manquian <emsayes@gmail.com>
2022-11-09 16:07:31 -0300

    Actualizacion 2.0 correcion packages previo subir a playstore

---
+++ app/src/main/java/com/asfventapp/ConsolaActivity.java
@@ -0,0 +1,76 @@
+package com.example.asfventapp;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+public class ConsolaActivity extends AppCompatActivity {
+
+    Spinner mem_consola;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_consola);
+        mem_consola = findViewById(R.id.idSpinner);
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.memoria_consola, android.R.layout.simple_spinner_item);
+
+        mem_consola.setAdapter(adapter);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.action_main:
+                Toast.makeText(this,"Home", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(ConsolaActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+                break;
+            case R.id.action_cabinet:
+                Toast.makeText(this,"Arcade Cabinet", Toast.LENGTH_SHORT).show();
+                Intent intent2 = new Intent(ConsolaActivity.this, CabinetActivity.class);
+                startActivity(intent2);
+                finish();
+                break;
+            case R.id.action_consola:
+                Toast.makeText(this,"Consola Arcade", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.action_perfil:
+                Toast.makeText(this,"Ingresar a tu cuenta", Toast.LENGTH_SHORT).show();
+                Intent intent4 = new Intent(ConsolaActivity.this, LoginActivity.class);
+                startActivity(intent4);
+                finish();
+                break;
+            case R.id.action_contactanos:
+                Toast.makeText(this,"Contactanos", Toast.LENGTH_SHORT).show();
+                Intent intent5 = new Intent(ConsolaActivity.this, MainContactanos.class);
+                startActivity(intent5);
+                finish();
+                break;
+            case R.id.action_visitanos:
+                Toast.makeText(this,"Visitanos", Toast.LENGTH_SHORT).show();
+                Intent intent6 = new Intent(ConsolaActivity.this, MapActivity.class);
+                startActivity(intent6);
+                finish();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
